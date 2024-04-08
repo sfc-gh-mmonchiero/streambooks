05e3051d0f83fb3d83b26c34876036571d660ab4
Matteo <matteo.monchiero@snowflake.com>
2024-04-08 16:48:12 -0400

    Import README

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from pathlib import Path
+
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
 
 @st.experimental_fragment
 def cell(key):
@@ -13,9 +17,12 @@ def main():
         st.session_state.my_locals = {}
 
     st.title("My Streambook")
+    intro_markdown = read_markdown_file("README.md")
+    st.markdown(intro_markdown, unsafe_allow_html=True)
+
 
     with st.sidebar:
-        rows = st.number_input("Number of rows", 3)
+        rows = st.number_input("Number of rows", 5)
     for i in range(rows):
         cell(i)
 
