4ebc5f2a58f0465e0277f5dc4aef35705f126239
Matteo <matteo.monchiero@snowflake.com>
2024-04-08 15:49:04 -0400

    checkpoint

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,32 +2,54 @@ import streamlit as st
 
 code_list = []
 
-@st.experimental_fragment
-def box(key):
-    # restore state
-    text = st.text_area("Type here", key=key)
-    if text:
-        exec(text)
-    # save state
 
-def box_co(key):
+@st.experimental_fragment
+def cell(key):
+    with st.container(border=True):
+        text = st.text_area("Type here", key=f"text_area{key}")
+        if text:
+            exec(text, None, st.session_state.my_locals)
+        st.button("Run cell", key=f"button{key}")
+
+def cell_list(key):
     text = st.text_area("Type here", key=key)
     if text:
         compiled = compile(text, "", "exec")
-        code_list.append(compiled)
+        placeholder = st.container(border=True)
+        code_list.append((compiled, placeholder))
 
+@st.experimental_fragment
+def frag_exec(e):
+    with e[1]:
+        exec(e[0], None, st.session_state.my_locals)
+
+def cell_co():
+    i = 0
+    while True:
+        text = st.text_area("Type here", key=i)
+        if text:
+            exec(text)
+            i = i + 1
+        else:
+            yield
 
 def main():
+    if 'my_locals' not in st.session_state:
+        st.session_state.my_locals = {}
+
+    st.title("My Streambook")
+
+    # this is cute, but not exactly a notebbok... sementic is driven by "clicks"
+    # cell_co().__next__()
 
     with st.sidebar:
         rows = st.number_input("Number of rows", 3)
-
-    st.title("My Streambook")
+        if st.button("Rerun"):
+            st.rerun()
     for i in range(rows):
-        box_co(i)
-    for e in code_list:
-        exec(e)
-    cose_list = []
+        cell(i)
+    #for e in code_list:
+    #   frag_exec(e)
 
 
 
